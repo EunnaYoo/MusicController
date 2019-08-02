0fdbd32edc0773fc142e9a42200bde2e7ed379b2
EunnaYoo <dmssk6788@gmail.com>
2019-08-02 14:45:29 +0900

    All Finish

--- MusicController/src/music/controller/AdminController.java
+++ MusicController/src/music/controller/AdminController.java
@@ -14,21 +14,25 @@ import music.service.Service;
 import music.view.EndView;
 
 public class AdminController {
-	
+
 	private static Logger logger = Logger.getLogger(AdminController.class.toString());
 	private static AdminController instance = new AdminController();
+
 	private AdminController(){};
+
 	public static AdminController getInstance() {
 		return instance;
 	}
-	
+
 	private static Service service = Service.getInstance();
 
 	static Scanner sc = new Scanner(System.in);
-	
-	//곡목 검색
+
+	// 곡목 검색
 	public void printSongList(String name) {
+		
 		logger.info("admin printSongList");
+		
 		try {
 			ArrayList<PrintSongDTO> songList = service.printSongList(name);
 			EndView.showSongList(songList);
@@ -42,16 +46,16 @@ public class AdminController {
 			PrintSongDTO aim = songList.get(indexList.indexOf(sc.nextInt()));
 
 			EndView.Message("1.듣기\t2.삭제\t0.종료");
-			
+
 			int req = sc.nextInt();
 			if (req == 1) {
-				logger.info("admin listen to song "+ aim.getId());
+				logger.info("admin listen to song " + aim.getId());
 				EndView.watchMovie(aim.getName(), aim.getSinger());
-			} else if(req==2){
-				logger.info("admin delete song "+ aim.getId());
+			} else if (req == 2) {
+				logger.info("admin delete song " + aim.getId());
 				service.deleteSong(aim.getId());
-			} else if(req==0) {
-				return ;
+			} else if (req == 0) {
+				return;
 			} else {
 				logger.warn("fail to find song");
 				EndView.failView("잘못된 입력값입니다.");
@@ -62,39 +66,43 @@ public class AdminController {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// 가수검색
 	public void getSingerList(String name) {
+		
 		logger.info("admin getSingerList");
+		
 		try {
 			ArrayList<SingerDTO> singerList = service.getSingerList(name);
-			
+
 			EndView.showSongList(singerList);
 			EndView.Message("곡 목록을 보고 싶은 가수의 번호를 선택해 주세요");
-			
+
 			SingerDTO aim = singerList.get(sc.nextInt() - 1);
-			
-			logger.info("admin find singer "+ aim.getId());
-			
+
+			logger.info("admin find singer " + aim.getId());
+
 			ArrayList<PrintSongDTO> songList = service.getSongListBySinger(aim.getId());
 			EndView.showSongList(songList);
 
 			EndView.Message("곡 번호를 입력해주세요");
 			PrintSongDTO aim2 = songList.get(sc.nextInt() - 1);
-			
-			logger.info("admin listen to song "+ aim2.getId());
+
+			logger.info("admin listen to song " + aim2.getId());
 			EndView.watchMovie(aim2.getName(), aim2.getSinger());
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void addSong(SongDTO song) {
+		
 		logger.info("admin addSong");
+		
 		try {
 			boolean result = service.addSong(song);
-			
+
 			if (result == false) {
 				logger.warn("admin fail to add song");
 				EndView.failView("저장실패");
@@ -114,9 +122,8 @@ public class AdminController {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void addSinger(SingerDTO singer) {
-		
 		try {
 			boolean result = service.addSinger(singer);
 			logger.info("admin add singer");
@@ -129,6 +136,7 @@ public class AdminController {
 			e.printStackTrace();
 		}
 	}
+
 	public void addSingersFromFile(String f) {
 		try {
 			logger.info("admin add singer from file");
@@ -140,43 +148,40 @@ public class AdminController {
 	}
 
 	public void addUser(UserDTO user) {
-		
+
 		boolean result;
-		
+
 		try {
 			result = service.addUser(user);
 			logger.info("admin add user");
-			
+
 			if (result) {
 				EndView.successView("등록되었습니다.");
 			} else {
 				EndView.failView("등록실패");
 				logger.warn("admin fail to add user");
 			}
-			
 		} catch (SQLException e) {
 			logger.warn(e);
 			e.printStackTrace();
 		}
 	}
 
-
 	public void addNewSong(String date) {
-		
+
 		boolean result;
-		
+
 		try {
 			result = service.addNewSong(date);
-			
+
 			logger.info("admin update new song chart");
-			
+
 			if (result) {
 				EndView.successView("최신차트 갱신 성공");
 			} else {
 				logger.warn("admin fail to update new song chart");
 				EndView.failView("최신차트 갱신 실패");
 			}
-			
 		} catch (SQLException e) {
 			logger.warn(e);
 			e.printStackTrace();
@@ -184,46 +189,45 @@ public class AdminController {
 	}
 
 	public void updatePopularChart() {
-		
+
 		try {
 			boolean result = service.updatePopularChart();
 			logger.info("admin update popular chart");
-			
+
 			if (result) {
 				EndView.successView("인기차트 갱신");
 			} else {
 				logger.info("admin fail to update popular chart");
 				EndView.failView("인기차트 갱신 실패");
 			}
-			
 		} catch (SQLException e) {
 			logger.warn(e);
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void getNew() {
 		try {
 			logger.info("admin get new song chart");
 			ArrayList<PrintSongDTO> songList = service.getNew();
 			EndView.showSongList(songList);
-			
+
 			EndView.Message("곡 번호를 입력해주세요");
 			PrintSongDTO aim = songList.get(sc.nextInt() - 1);
-			
-			logger.info("admin select song"+ aim.getId());
-			
+
+			logger.info("admin select song" + aim.getId());
+
 			EndView.Message("1.듣기\t2.삭제\t0.종료");
-			
+
 			int req = sc.nextInt();
 			if (req == 1) {
-				logger.info("admin listen to song "+ aim.getId());
+				logger.info("admin listen to song " + aim.getId());
 				EndView.watchMovie(aim.getName(), aim.getSinger());
-			} else if(req==2){
-				logger.info("admin delete song "+ aim.getId());
+			} else if (req == 2) {
+				logger.info("admin delete song " + aim.getId());
 				service.deleteSong(aim.getId());
-			} else if(req==0) {
-				return ;
+			} else if (req == 0) {
+				return;
 			} else {
 				logger.warn("fail to find song");
 				EndView.failView("잘못된 입력값입니다.");
@@ -234,7 +238,7 @@ public class AdminController {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void getPopular() {
 
 		try {
@@ -245,16 +249,16 @@ public class AdminController {
 			PrintSongDTO aim = songList.get(sc.nextInt() - 1);
 
 			EndView.Message("1.듣기\t2.삭제\t0.종료");
-			
+
 			int req = sc.nextInt();
 			if (req == 1) {
-				logger.info("admin listen to song "+ aim.getId());
+				logger.info("admin listen to song " + aim.getId());
 				EndView.watchMovie(aim.getName(), aim.getSinger());
-			} else if(req==2){
-				logger.info("admin delete song "+ aim.getId());
+			} else if (req == 2) {
+				logger.info("admin delete song " + aim.getId());
 				service.deleteSong(aim.getId());
-			} else if(req==0) {
-				return ;
+			} else if (req == 0) {
+				return;
 			} else {
 				logger.warn("fail to find song");
 				EndView.failView("잘못된 입력값입니다.");
--- MusicController/src/music/view/StartView.java
+++ MusicController/src/music/view/StartView.java
@@ -11,25 +11,33 @@ import music.model.dto.UserDTO;
 public class StartView {
 	
 	public static void main(String[] args) {
+		
 		boolean req = false;
 		
 		Scanner sc = new Scanner(System.in);
 		EndView.Message("id를 입력해주세요");
 		String logInId = sc.next();
+		
 		if(logInId.equals("admin")) {
+			
 			AdminController controller = AdminController.getInstance();
+			
 			while(!req) {
+				
 				EndView.Message("1. 곡 검색\t2. 가수 검색\t3. 곡 추가\t4. 곡 파일 추가\t"
-						+ "5. 가수 추가\n6.가수 파일 추가\t7.유저 추가\t8. 최신차트 갱신\t9. 인기차트 갱신\t0. 종료");
+								+ "5. 가수 추가\n6.가수 파일 추가\t7.유저 추가\t8. 최신차트 갱신\t9. 인기차트 갱신\t0. 종료");
+				
 				EndView.Message("사용할 기능의 번호를 입력해주세요");
 				int reqNumber = sc.nextInt();
-				if(reqNumber ==1) {
+				if (reqNumber == 1) {
 					EndView.Message("곡 명을 입력해주세요");
 					controller.printSongList(sc.next());
-				}else if(reqNumber ==2) {
+					
+				} else if(reqNumber == 2) {
 					EndView.Message("가수명을 입력해주세요");
 					controller.getSingerList(sc.next());
-				}else if(reqNumber ==3) {
+					
+				} else if(reqNumber == 3) {
 					EndView.Message("곡 번호");
 					int id=sc.nextInt();
 					EndView.Message("곡명");
@@ -39,17 +47,21 @@ public class StartView {
 					EndView.Message("발매일(yyyy.mm)");
 					String date=sc.next();
 					controller.addSong(new SongDTO(id,name,singer,date,0));
-				}else if(reqNumber==4) {
-					controller.addSongFromFile("C:\\Users\\Playdata\\Desktop\\git\\MusicController\\songLista.txt");
-				}else if(reqNumber==5) {
+					
+				} else if(reqNumber == 4) {
+					controller.addSongFromFile("C:\\Git\\MusicController\\songLista.txt");
+					
+				} else if(reqNumber == 5) {
 					EndView.Message("가수 번호");
 					int id=sc.nextInt();
 					EndView.Message("가수명");
 					String name=sc.next();
 					controller.addSinger(new SingerDTO(id,name));
-				}else if(reqNumber==6) {
-					controller.addSingersFromFile("C:\\Users\\Playdata\\Desktop\\git\\MusicController\\singerList.txt");
-				}else if(reqNumber==7) {
+					
+				} else if(reqNumber == 6) {
+					controller.addSingersFromFile("C:\\Git\\MusicController\\singerList.txt");
+					
+				} else if(reqNumber == 7) {
 					EndView.Message("id");
 					String id=sc.next();
 					EndView.Message("이름");
@@ -57,55 +69,56 @@ public class StartView {
 					EndView.Message("비밀번호");
 					String password=sc.next();
 					controller.addUser(new UserDTO(id,name,password));
-				}else if(reqNumber==8) {
+					
+				} else if(reqNumber == 8) {
 					EndView.Message("발매일(yyyy.mm)");
 					String date=sc.next();
 					controller.addNewSong(date);
 					controller.getNew();
-				}else if(reqNumber==9) {
+					
+				} else if(reqNumber==9) {
 					controller.updatePopularChart();
 					controller.getPopular();
-				}else if(reqNumber==0){
+					
+				} else if(reqNumber==0){
 					req=true;
-				}else {
+					
+				} else {
 					EndView.failView("잘못된 요청입니다");
 				}
 			}
 			
-		}else {
+		} else {
+			
 			UsersController controller = UsersController.getInstance();
+			
 			while(!req) {
 				EndView.Message("1. 곡 검색\t2. 가수 검색\t3. 내 목록\t 4. 신곡\t5. 인기차트\n0. 종료");
 				int reqNumber=sc.nextInt();
-				if(reqNumber==1) {
+				if(reqNumber == 1) {
 					EndView.Message("곡 명을 입력해주세요");
 					controller.printSongList(sc.next(),logInId);
-				}else if(reqNumber==2) {
+					
+				} else if(reqNumber == 2) {
 					EndView.Message("가수 명을 입력해주세요");
 					controller.getSingerList(sc.next());
-				}else if(reqNumber==3) {
+					
+				} else if(reqNumber == 3) {
 					controller.getMyList(logInId);
-				}else if(reqNumber==4) {
+					
+				} else if(reqNumber == 4) {
 					controller.getNew();
-				}else if(reqNumber==5) {
+					
+				} else if(reqNumber == 5) {
 					controller.getNew();
-				}else if(reqNumber==0) {
+					
+				} else if(reqNumber == 0) {
 					req=true;
-				}else {
+					
+				} else {
 					EndView.Message("잘못된 요청입니다");
 				}
 			}
 		}
-		
-//		controller.updatePopularChart();
-//		controller.getMyList(1);
-//		controller.addUser(new UserDTO(1,"최성국","0000"));
-//		controller.getSingerList("나");
-//		controller.addSingersFromFile("C:\\Git\\MusicController\\singerList.txt");
-//		controller.addSongFromFile("C:\\Git\\MusicController\\songLista.txt");
-//		controller2.getPopular();
-//		controller.addNewSong("2019.05");
-//		controller2.getNew();
-//		controller.printSongList("귀");
 	}
 }
